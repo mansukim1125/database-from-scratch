5c4b1890ac592df702ef102530f1c5c99ded5a3a
mansukim1125 <mansukim1125@gmail.com>
2022-09-03 12:49:25 +0900

    for github push

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+    u_int32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct {
     char* buffer;
     size_t buffer_length;
@@ -26,6 +35,7 @@ typedef enum {
 
 typedef struct {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
 InputBuffer* new_input_buffer() {
@@ -67,6 +77,16 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(
+            input_buffer->buffer,
+            "insert %d %s %s",
+            &(statement->row_to_insert.id),
+            statement->row_to_insert.username,
+            statement->row_to_insert.email
+        );
+        if (args_assigned < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
